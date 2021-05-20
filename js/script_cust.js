c2781f5cb0834d6ae2e7874c22af7e0c34d48a66
Mainak <mainakme2140@gmail.com>
2021-05-20 20:07:32 +0700

    form

--- js/script_cust.js
+++ js/script_cust.js
@@ -1,19 +1,13 @@
-function translate(){
-    var url = window.location.href;
-    console.log(url)
-    if(url.startsWith("https://translate.google.com")){
-        var newurl = "http://crashxz.github.io";
-    }
-    else{
-        var newurl = "https://translate.google.com/translate?sl=en&tl=ru&u="+url;
-    }
-    
-    location.replace(newurl);
+function SubForm(){
+    $.ajax({
+        url:'https://api.apispreadsheets.com/data/12678/',
+        type:'post',
+        data:$("#ContactUs").serializeArray(),
+        success: function(){
+          alert("Form Data Submitted :)")
+        },
+        error: function(){
+          alert("There was an error :(")
+        }
+    });
 }
-
-$(function() {
-    if(document.getElementsByClassName('goog-te-banner-frame skiptranslate')[0] !== undefined) {
-        document.getElementsByClassName('goog-te-banner-frame skiptranslate')[0].style.display  = 'none';
-        document.body.style.top = '0px';
-      }
-});
